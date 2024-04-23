9d4113cfff5f21d3a25c4294fe77a44bded4e940
Amanuel Desalegn <amandesalegnb@gmail.com>
2024-04-23 22:57:57 +0300

    post crud added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\WebsiteController;
 use Illuminate\Http\Request;
@@ -9,3 +10,6 @@ Route::resource('websites', WebsiteController::class);
 Route::get('all-websites', [WebsiteController::class, 'get_all']);
 
 Route::post('subscribe', [SubscriptionController::class, 'store']);
+
+Route::resource('posts', PostController::class);
+Route::get('all-posts', [PostController::class, 'get_all']);
